da135e8ad3dc98c0c0d7c56ffc972e4283c1827f
William <minecraftmason09@gmail.com>
2020-09-19 20:45:48 -0700

    third commit

--- index.php
+++ index.php
@@ -9,16 +9,18 @@ $files2 = glob("./posts/" . "*");
 //Loop through each text file (post) then neatly echo out the contents:
 foreach ($files2 as $post) {
     $content = file_get_contents($post);
-    $contentArray = explode("-split-",$content,3);
+    $contentArray = explode("-split-",$content,4);
     $title = $contentArray[0];
     $text = $contentArray[1];
     $username = $contentArray[2];
+    $id = $contentArray[3];
 
     echo "
     <div class='center width600'>
         <div class='post'>
             <p class='mainFont titleFont'>$title</p>
             <p class='mainFont'>$text</p>
+            <p class='mainFont'>$id</p>
             <p class='mainFont usernameFont'>$username</p>
         </div>
     </div>";
